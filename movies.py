17b9e12b34403c1c4e0262ee58f4450f62df9dd4
Kingsley <divine10646@gmail.com>
2023-07-27 14:36:35 +0100

    fixed get movie by id

--- movies.py
+++ movies.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def get_movie_by_id(csv_file_path, movie_id):
     df = pd.read_csv(csv_file_path)
-    movie = df.loc[df['id'] == movie_id]
+    movie = df.loc[df['id'] == int(movie_id)]
     return movie.to_dict(orient='records')
 
 def get_movie_by_name(csv_file_path, movie_name):
