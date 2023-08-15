1cf5ae99d93aa5bf57546a841a40c04ef4f9c147
Kingsley <divine10646@gmail.com>
2023-08-15 17:31:09 +0100

    updated db credentials from environment

--- db/engine.py
+++ db/engine.py
@@ -1,11 +1,20 @@
+import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
 
-# Replace these values with your database configuration
 db_user = 'root'
 db_password = ''
 db_host = 'localhost'
 db_name = 'db_agency'
 
+if os.getenv('ENVIRONMENT') == 'production':
+    db_user = os.getenv('DB_USERNAME')
+    db_password = os.getenv('DB_PASSWORD')
+    db_host = 'localhost'
+    db_name = 'db_agency'
+
+print(db_user)
 # Create a SQLAlchemy engine
 engine = create_engine(f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}')
 
